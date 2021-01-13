50a3aa85350573ed1cdae9ab99a4f7acfa0f9fcb
MEGHANA221999 <44064988+MEGHANA221999@users.noreply.github.com>
2021-01-13 14:01:22 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ec2_pro" {
     ami ="${var.ami}"
     instance_type = "${var.instance_type}"
     key_name= "06jan"
-    security_groups= ["${aws_security_group.ALLTRAFFIC.name}"]
+    
     
 }
 
